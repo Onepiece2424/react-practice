f6a0d52bcd4c958982631c4ebf0a7e73622baff0
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-06-18 11:04:37 +0900

    数値が0の時、絵文字表示させなくする機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
@@ -11,12 +11,13 @@ const App = () => {
     setFaceShowFlag(!faceShowFlag);
   };
 
-  if (num % 3 === 0) {
-    faceShowFlag || setFaceShowFlag(true);
-  } else {
-    faceShowFlag && setFaceShowFlag(false);
+  if (num > 0) {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
   }
-
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
